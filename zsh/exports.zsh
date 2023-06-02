f6b944e007868f68a4c0cc2b5d2d06e7f65a6031
Johnny Liu <johnliu@nvidia.com>
2023-06-02 14:12:06 +0800

    Add snap path into PATH

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,3 +4,6 @@ export EDITOR="nvim"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+
+# Include snap path
+export PATH="/snap/bin:$PATH"
